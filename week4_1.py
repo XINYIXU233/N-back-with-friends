faf2215410462ac7bc958def1c9cea151cb82ecb
TeaforTwo1011 <92511991+TeaforTwo1011@users.noreply.github.com>
2023-01-09 15:22:24 +0100

    Update week4_1.py

--- week4_1.py
+++ week4_1.py
@@ -27,8 +27,8 @@ import nsequence_generator
 #1.Preparation
 # record information
 ID = 1 #please enter the participant ID here. 
-item_number = 25 # you can define how many trials to present in each block by chaning the number. Here we are going to present 25 letters in each block in total
-correct_number = 5 # you can define how many many trials are correct in each block. Here we have 5 correct trials in each block
+item_number = 7 # you can define how many trials to present in each block by chaning the number. Here we are going to present 25 letters in each block in total
+correct_number = 3 # you can define how many many trials are correct in each block. Here we have 5 correct trials in each block
 
 # create the window
 win = visual.Window(size = (800, 600), units = 'pix', color = "black")
@@ -91,10 +91,10 @@ for n in range(1,4):
     core.wait(2)
     
     correct_n = 0         #count correct trials
-    hit_n = 1             #count hit
-    false_alarm_n = 2     #count false alarm
-    reject_n = 3          #count correct reject
-    miss_n = 4            #count miss
+    hit_n = 0             #count hit
+    false_alarm_n = 0     #count false alarm
+    reject_n = 0          #count correct reject
+    miss_n = 0            #count miss
 
     # display the letter sequence
     # show letter sequence: each letter show for 0.5s and break for 2.5s
@@ -113,9 +113,12 @@ for n in range(1,4):
             if trial_list_temp[1] == trial_list_temp[5]:
                 trial_list_temp[i].append(1) # record correctness, correct =1, otherwise 0
                 trial_list_temp[i].append(3) #record response based on signal detection theory, hit/false alarm/reject/miss =1/2/3/4
+                correct_n = correct_n + 1
+                reject_n = reject_n + 1
             else:
                 trial_list_temp[i].append(0)
                 trial_list_temp[i].append(2)
+                false_alarm_n = false_alarm_n + 1
         else:
             key = keys[0]
             trial_list_temp[i].append("Y") # record the key pressed
@@ -123,11 +126,13 @@ for n in range(1,4):
             trial_list_temp[i].append(key.rt) # record the reaction time
             if trial_list_temp[1] == trial_list_temp[5]:
                 trial_list_temp[i].append(1)
-                correct_n = correct_n+1
                 trial_list_temp[i].append(1)
+                correct_n = correct_n + 1
+                hit_n = hit_n + 1
             else:
                 trial_list_temp[i].append(0)
                 trial_list_temp[i].append(4)
+                miss_n = miss_n + 1
     
     answer_list.append(answer_list_temp)
     n_sequence.append(n_sequence_temp)
@@ -145,9 +150,18 @@ data = [['ID', 'Anser_List','Letter_Display','Block', 'Key_Pressed', 'Response',
 for sublist in trial_list:
     data.append(sublist)
     
+data_rates = [['correct','hit','false alarm','reject','miss']]
+data_rates.append([correct_n/item_number, hit_n/item_number, false_alarm_n/item_number,
+            reject_n/item_number, miss_n/item_number])
 df = pd.DataFrame(data)
+df_rates = pd.DataFrame(data_rates)
 print(df) #test the data structure 
+print(df_rates)
 
 filename = f'sub-{ID}.xlsx' # data filename for each sub.
 filepath = '/Users/luoqi/Desktop/' #path of folder where you save data
 df.to_excel(f'{filename}', index=False) #change it to your own directory
+
+filename_rates = f'rates-sub-{ID}.xlsx' # data filename for each sub.
+filepath = '/Users/luoqi/Desktop/' #path of folder where you save data
+df.to_excel(f'{filename_rates}', index=False) #change it to your own directory
